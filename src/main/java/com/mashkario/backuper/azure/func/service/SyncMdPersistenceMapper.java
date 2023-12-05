36889e18ddc5e04bcc336418f8bf36b7a7687b83
roman-ks <roman-ks@users.noreply.github.com>
2023-12-05 15:43:34 +0200

    base64 encode key as url encoding still produces error in portal

--- src/main/java/com/mashkario/backuper/azure/func/service/SyncMdPersistenceMapper.java
+++ src/main/java/com/mashkario/backuper/azure/func/service/SyncMdPersistenceMapper.java
@@ -2,21 +2,26 @@ package com.mashkario.backuper.azure.func.service;
 
 import com.mashkario.backuper.azure.func.dto.SyncMdDto;
 
-import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 public class SyncMdPersistenceMapper {
 
     public SyncMdPersistenceDto map(SyncMdDto mdDto) {
         String driveId = mdDto.driveId();
-        String encodePath = URLEncoder.encode(mdDto.path(), StandardCharsets.UTF_8);
+        String path = mdDto.path();
+        String rawId = driveId + "^" + path;
+        // base64 encode as path can contain invalid chars
+        String base64Id = Base64.getEncoder()
+                .encodeToString(rawId.getBytes(StandardCharsets.UTF_8));
 
-        return new SyncMdPersistenceDto(driveId + "^" + encodePath,
+        return new SyncMdPersistenceDto(base64Id,
                 mdDto.fileName(),
                 mdDto.path(),
                 driveId,
                 mdDto.driveName(),
                 mdDto.lastModified());
     }
+
 
 }
